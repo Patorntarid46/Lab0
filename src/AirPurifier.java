cf2024fc56911c6f54d405d13a44dd7a18795074
Patorntarid Chaivong <patorntarid_c@cmu.ac.th>
2022-12-09 21:19:23 +0700

    AirPurifier commit

--- src/AirPurifier.java
+++ src/AirPurifier.java
@@ -1,10 +1,72 @@
 public class AirPurifier {
-    int Price;
-    int AirQuality;
-    String Color;
-    void printairquality(){
-        
+    private boolean power=false;
+    private int speed=0;
+    private int sellCount;
+    private int price;
+    private static int discount=0;
+    String model;
+    String colour;
+
+    AirPurifier(String model, String colour, int price,int count){
+        this.model=model;
+        this.colour=colour;
+        this.price=price;
+        this.sellCount=count;
+    }
+    public void setPower(){
+        if(this.power==false)this.power=true;
+        else this.power=false;
     }
 
+    public boolean status(){
+        return this.power;
+    }
 
+    public int speed(int increment){
+        if(increment<=100&&increment>=0){
+            this.speed=increment;
+            if(increment!=0) this.power=true;
+            else this.power=false;
+        }
+        return this.speed;
+    }
+
+    public int checkSpeed(){
+        return this.speed;
+    }
+
+    static public void sortAirPurifier(AirPurifier[] ap){
+        for(int i=0;i< ap.length;i++){
+            for (int j=i;j< ap.length;j++){
+                if(ap[i].sellCount<ap[j].sellCount){
+                    AirPurifier x= ap[i];
+                    ap[i]=ap[j];
+                    ap[j]=x;
+                }
+            }
+        }
+        for (int i=0;i< ap.length;i++) System.out.println(i+1+"# => "+ap[i].model);
+    }
+
+    public void sellCountUp(){
+        this.sellCount++;
+        System.out.println(this.model+" à¸¡à¸µà¸à¸²à¸£à¸‚à¸²à¸¢!!!");
+    }
+
+    public int sellCount(){
+        return this.sellCount;
+    }
+
+    public static int getDiscount(int dc){
+        if(dc<=100&&dc>=0)
+            discount=dc;
+        return discount;
+    }
+
+    public static int checkDiscount(){
+        return discount;
+    }
+    public int checkPrice(){
+        return this.price-(this.price*discount/100);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,54 @@
-public class Main {
-    public static void main(String[] args) {
-        AirPurifier
+public class Lab2 {
 
-//       System.out.println("Hello world!");
-//        Car myCar1 = new Car();
-//        Car myCar2 = new Car(_speed:30);
-////        System.out.println(myCar.year); printf , จุดทำหน้าที่คล้ายๆ 's
-////        myCar.year = 1997;
-////        System.out.println(myCar.year); printf , จุดทำหน้าที่คล้ายๆ 's
-////        System.out.println(myCar.speed);
-//        myCar.printSpeed();
+    public static void main(String[] args){
+        //ประกาศObject AirPurifier
+        AirPurifier f = new AirPurifier("Samsung", "Black",12000,12000);
+        AirPurifier a = new AirPurifier("Vivo", "White",900,90000);
+        AirPurifier b = new AirPurifier("Boegy", "Black",300000,500);
+        AirPurifier c = new AirPurifier("Fifa", "Pink",100000,1500);
+        AirPurifier d = new AirPurifier("Apple", "Red",150000,10);
+        AirPurifier[] arr ={a,b,c,d,f};
 
+        //Test ปุ่มเปิด-ปิด
+        System.out.println(f.status());
+        f.setPower();
+        System.out.println(f.status());
+        f.setPower();
+        System.out.println(f.status());
+        System.out.println("------------------------------------");
 
+        //Test speedพัดลมของAirPurifier
+        System.out.println(f.status());
+        System.out.println("Fan speed : "+f.checkSpeed());
+        System.out.println("Set speed : "+f.speed(25));
+        System.out.println("Fan speed : "+f.checkSpeed());
+        System.out.println(f.status());
+        System.out.println("Set speed : "+f.speed(0));
+        System.out.println("Fan speed : "+f.checkSpeed());
+        System.out.println(f.status());
+        System.out.println("------------------------------------");
+
+        //Test ระบบยอดขาย
+        System.out.println("ยอดขายของ"+f.model+" = "+f.sellCount());
+        System.out.println("ยอดขายของ"+c.model+" = "+c.sellCount());
+        f.sellCountUp();
+        f.sellCountUp();
+        c.sellCountUp();
+        System.out.println("ยอดขายของ"+f.model+" = "+f.sellCount());
+        System.out.println("ยอดขายของ"+c.model+" = "+c.sellCount());
+        System.out.println("------------------------------------");
+
+        //เรียงลำดับยอดขายของAirPurifier จากมากไปน้อย
+        AirPurifier.sortAirPurifier(arr);
+        System.out.println("------------------------------------");
+
+        //Test ระบบราคา
+        System.out.println("ลดราคา "+AirPurifier.checkDiscount()+" %");
+        System.out.println(a.model+" ราคา "+a.checkPrice());
+        System.out.println(b.model+" ราคา "+b.checkPrice());
+        System.out.println("ลดราคา "+AirPurifier.getDiscount(10)+" %");
+        System.out.println(a.model+" ลดราคาเหลือ "+a.checkPrice());
+        System.out.println(b.model+" ลดราคาเหลือ "+b.checkPrice());
+        System.out.println("------------------------------------");
     }
 }
